c496a4a21c439a3c6a907e6a277609ebca9dca25
VariKXOFF <varikxoff@yandex.ru>
2021-10-16 20:58:18 +0300

    start project

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require("express")
 const sass = require("node-sass-middleware")
+const path = require('path');
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(express.urlencoded(true))
 app.use(express.json())
 app.use("/", require("./server/router"))
-app.use(express.static("./public"))
 app.use(
     sass({
         src: __dirname + "/public/stylesheet/scss",
-        dest: __dirname + "/public/stylesheet/css",
-        debug: true
+        dest: path.join('/css'),
+        outputStyle: 'compressed'
     })
 )
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", "pug")
 app.set("views", "./server/templates")
